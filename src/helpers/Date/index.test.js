b760cf8bc595dcf5bd4f47f005b956936028826e
Redouane Bouchmaa <red.bouchmaa.dev@gmail.com>
2023-09-21 12:41:16 +0200

    Ajout test date

--- src/helpers/Date/index.test.js
+++ src/helpers/Date/index.test.js
@@ -1,15 +1,24 @@
 /**
  * 
  */
+import { getMonth } from "./index";
 
 describe("Date helper", () => {
     describe("When getMonth is called", () => {
         it("the function return janvier for 2022-01-01 as date", () => {
+            const date = new Date("2022-01-01")
+            const month = getMonth(date)
+
+            expect(month).toBe("janvier")
             
             // to implement
         });
         it("the function return juillet for 2022-07-08 as date", () => {
             // to implement
+            const date = new Date("2022-07-08")
+            const month = getMonth(date)
+
+            expect(month).toBe("juillet")
         });
     });
 })
